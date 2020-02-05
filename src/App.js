e1d559bb46ed20986f18cede1075407d7389d742
“tatsuki0091” <rossosalvatore@icloud.com>
2020-02-05 20:14:26 +0900

    カウントアプリ作成

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(count +1)
+  const decrement = () => setCount(count -1)
+
+  const increment2 = () => setCount(previousCount =>  previousCount +1)
+  const decrement2 = () => setCount(previousCount =>  previousCount -1)
+
+  const reset = () => setCount(0)
+  const multiply = () => setCount(previousCount =>  previousCount * 2)
+  const devideThree = () => setCount(previousCount =>  {
+    if(previousCount%3 === 0) {
+      setCount(previousCount/3)
+    } else {
+      setCount(previousCount)
+    }
+  })
+
   return (
+    <>
     <div>
-      Hello
+      count: {count}
     </div>
+    <div>
+    <button onClick={decrement}>-1</button>
+    <button onClick={increment}>+1</button>
+    </div>
+    <div>
+    <button onClick={decrement2}>-1</button>
+    <button onClick={increment2}>+1</button>
+    </div>
+    <div>
+    <button onClick={reset}>Reset</button>
+    <button onClick={multiply}>*2</button>
+    <button onClick={devideThree}>3の倍数のときだけ3で割る</button>
+    </div>
+    
+    </>
+    
   );
 }
 
