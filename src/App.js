37eb0161ac1436cb7786364a1e1d265d70ad19ca
“tatsuki0091” <rossosalvatore@icloud.com>
2020-02-09 17:07:15 +0900

    複数の状態を管理

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
 import React, { useState } from 'react';
 
+const App = props => {
 
-const App = () => {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment = () => setCount(count +1)
-  const decrement = () => setCount(count -1)
-
-  const increment2 = () => setCount(previousCount =>  previousCount +1)
-  const decrement2 = () => setCount(previousCount =>  previousCount -1)
-
-  const reset = () => setCount(0)
-  const multiply = () => setCount(previousCount =>  previousCount * 2)
-  const devideThree = () => setCount(previousCount =>  {
-    if(previousCount%3 === 0) {
-      setCount(previousCount/3)
-    } else {
-      setCount(previousCount)
-    }
-  })
-
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
   return (
     <>
-    <div>
-      count: {count}
-    </div>
-    <div>
-    <button onClick={decrement}>-1</button>
-    <button onClick={increment}>+1</button>
-    </div>
-    <div>
-    <button onClick={decrement2}>-1</button>
-    <button onClick={increment2}>+1</button>
-    </div>
-    <div>
-    <button onClick={reset}>Reset</button>
-    <button onClick={multiply}>*2</button>
-    <button onClick={devideThree}>3の倍数のときだけ3で割る</button>
-    </div>
-    
+     <p>現在の{name}は、{price}</p>
+     <button onClick={()=>setPrice(price + 1)}>+1</button>
+     <button onClick={()=>setPrice(price - 1)}>-1</button>
+     <button onClick={reset}>Reset</button>
+     <input value={name} onChange={e => setName(e.target.value)}/>
     </>
-    
   );
+};
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
-export default App
+export default App;
