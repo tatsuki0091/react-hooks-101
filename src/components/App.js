94e328b95224f288c97e7387e16482b14f782a2c
“tatsuki0091” <rossosalvatore@icloud.com>
2020-02-13 20:06:27 +0900

    eventコンポーネント作成

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,52 @@
-import React, {useReducer, useState} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import reducer from '../reducers';
-
+import React, { useReducer, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import reducer from "../reducers";
+import Event from "./Event";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, [])
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
   const addEvent = e => {
-    e.preventDefault()
+    e.preventDefault();
     dispatch({
-      type: 'CREATE_EVENT',
+      type: "CREATE_EVENT",
       title,
       body
-    })
+    });
 
-    setTitle('')
-    setBody('')
-  }
+    setTitle("");
+    setBody("");
+  };
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}></input>
+          <input
+            className="form-control"
+            id="formEventTitle"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          ></input>
         </div>
         <div className="form-group">
           <label htmlFor="formEventBody">ボディ</label>
-          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}></textarea>
+          <textarea
+            className="form-control"
+            id="formEventBody"
+            value={body}
+            onChange={e => setBody(e.target.value)}
+          ></textarea>
         </div>
-        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button className="btn btn-primary" onClick={addEvent}>
+          イベントを作成する
+        </button>
+        <button className="btn btn-danger">
+          全てのイベントを削除する
+        </button>
       </form>
 
       <h4>イベント一覧</h4>
@@ -46,11 +60,11 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-
+          {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
         </tbody>
       </table>
     </div>
   );
-}
+};
 
-export default App
+export default App;
